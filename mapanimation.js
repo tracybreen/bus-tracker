4000c6313af9c2e4936379b6f0fd7d370c4b97da
Tracy Breen <iamtracybreen@gmail.com>
2021-10-02 23:01:18 -0400

    adds marker and click functionality

--- mapanimation.js
+++ mapanimation.js
@@ -14,10 +14,8 @@ const busStops = [
   [-71.118625, 42.374863],
 ];
 
-// TODO: add your own access token
-mapboxgl.accessToken = '';
+mapboxgl.accessToken = 'pk.eyJ1IjoidHJhY3licmVlbiIsImEiOiJja3RqZXhzOWYwaHR5MnVvMGcwMDBoeGlsIn0.ltMQGZZsA33cwfpqv40J1Q';
 
-// This is the map instance
 let map = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/mapbox/streets-v11',
@@ -25,17 +23,14 @@ let map = new mapboxgl.Map({
   zoom: 14,
 });
 
-// TODO: add a marker to the map at the first coordinates in the array busStops. The marker variable should be named "marker"
+let marker = new mapboxgl.Marker().setLngLat([-71.092761, 42.357575]).addTo(map);
 
-// counter here represents the index of the current bus stop
 let counter = 0;
 function move() {
-  // TODO: move the marker on the map every 1000ms. Use the function marker.setLngLat() to update the marker coordinates
-  // Use counter to access bus stops in the array busStops
-  // Make sure you call move() after you increment the counter.
-}
-
-// Do not edit code past this point
-if (typeof module !== 'undefined') {
-  module.exports = { move };
+  setTimeout(() => {
+    if (counter >= busStops.length) return;
+    marker.setLngLat(busStops[counter]);
+    counter++;
+    move();
+  }, 1000);
 }
